ba8a33ebf6afbdc2e1048dd67a9bb775951df1b2
ankylosaurus11 <dylanjkubic@gmail.com>
2024-06-18 06:14:10 -0400

    update textnode test for repr

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,14 +4,15 @@ from textnode import TextNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", "bold", "test.com")
-        node2 = TextNode("This is a text node", "bold", "test.com")
+        node = TextNode("This is a text node", "bold", "hi")
+        node2 = TextNode("This is a text node", "bold", "hi")
         self.assertEqual(node, node2)
 
     def test_repr(self):
-        node = TextNode("This is a text node", "bold")
+        node = TextNode("This is a text node", "bold", "test.com")
         node2 = TextNode("This is a text node", "bold", "test.com")
-        self.__repr__(node)
+        expected_repr = "TextNode(text='This is a text node', text_type='bold', url='test.com')"
+        self.assertEqual(repr(node), expected_repr)
 
 if __name__ == "__main__":
     unittest.main()
