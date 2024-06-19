68f5e58105159fbb975b0d7cfb2ba57a4dc0a385
ankylosaurus11 <dylanjkubic@gmail.com>
2024-06-19 19:42:54 -0400

    fixed LeafNode call/super and added ParentNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,13 +18,24 @@ class HTMLNode:
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag, value):
-        super().__init__(tag)
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
         if value == "":
-            raise ValueError("No value for LeafNode")
+            raise ValueError("No value provided")
         self.value = value
 
     def to_html(self):
         if not self.tag:
-            return value
+            return self.value
         return f"<{self.tag}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+        if children == "":
+            raise ValueError("No children provided")
+        if tag == "":
+            raise ValueError("No tag provided")
+        
+    def to_html(self):
+        
