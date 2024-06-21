31d81d294b0fe5c65da89cde33e5e21fe7fb9dfb
ankylosaurus11 <dylanjkubic@gmail.com>
2024-06-20 20:10:50 -0400

    create a ParentNode unittest

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,6 +2,7 @@ import unittest
 
 from htmlnode import HTMLNode
 from htmlnode import LeafNode
+from htmlnode import ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_prop(self):
@@ -18,6 +19,16 @@ class TestLeafNode(unittest.TestCase):
     def test_leaf_without_value(self):
         with self.assertRaises(ValueError):
             LeafNode(tag="p", value="")
+
+class TestParentNode(unittest.TestCase):
+    def test_parent(self):
+        leaf1 = LeafNode("b", "Bold text")
+        leaf2 = LeafNode(None, "Normal text")
+        leaf3 = LeafNode("i", "italic text")
+        leaf4 = LeafNode(None, "Another normal text")
+        parent = ParentNode("p", [leaf1, leaf2, leaf3, leaf4])
+        expected_parent = '<p><b>Bold text</b>Normal text<i>italic text</i>Another normal text</p>'
+        self.assertEqual(parent.to_html(), expected_parent)
 
 if __name__ == "__main__":
     unittest.main()
